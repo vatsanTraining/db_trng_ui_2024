66564ceac6b7576264f080e410fd29840cf8ff6a
srivatsan <vatsan_k@hotmail.com>
2024-07-26 12:30:40 +0530

    Javascript task1

---
+++ scripts/order.js
@@ -0,0 +1,12 @@
+
+var orderObj = {}
+
+orderObj.addHeading = function(){
+   var headElement =document.querySelector("#majheading");
+   headElement.innerHTML="Shiv Online Store"
+   headElement.setAttribute('class','centerText')
+
+}
+
+
+orderObj.addHeading();
