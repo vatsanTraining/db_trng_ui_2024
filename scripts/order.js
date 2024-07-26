73bd06cfc2bb5b297e8f5f064817a1000b1e6e96
srivatsan <vatsan_k@hotmail.com>
2024-07-26 12:52:32 +0530

    event handling

--- scripts/order.js
+++ scripts/order.js
@@ -8,5 +8,45 @@ orderObj.addHeading = function(){
 
 }
 
+// create a method addImage and add a image to header element 
+
+orderObj.addImage = function(imageRef){
+    var headElement =document.querySelector("header");
+    var imageElement =document.createElement("img");
+    imageElement.setAttribute('src',imageRef)
+    imageElement.setAttribute('class','logo')
+    imageElement.setAttribute('id','logo')
+    
+
+    headElement.append(imageElement)
+ }
+
+ // Attach an moveover to the image and change the border
+// use an annonymous call back function and also a named call back function
+
+orderObj.handleMouseOver = function(){
+
+    var imgElement =document.querySelector("#logo");
+
+    imgElement.setAttribute('style','border:5px solid red')
+
+}
+orderObj.attchEventToLogo = function(){
+
+    var imgElement =document.querySelector("#logo");
+
+    // imgElement.addEventListener('mouseover',function(){
+
+    //     imgElement.setAttribute('style','border:5px solid red')
+    // })
+
+    // observe the method is not having circle braces ()
+    imgElement.addEventListener('mouseover',orderObj.handleMouseOver)
+}
+
 
 orderObj.addHeading();
+orderObj.addImage('images/logo.png')
+orderObj.attchEventToLogo();
+console.log(orderObj.handleMouseOver)
+//console.log(orderObj.handleMouseOver())
